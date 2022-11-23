9a7a20ba0d8bc3c1dc12d156ea44847cae354595
Johora-Juhi <zohrajuhi1012@gmail.com>
2022-11-24 01:14:08 +0600

    firebase

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layouts/Main/Main";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login/Login";
+import Signup from "../../Pages/Login/Signup/Signup";
+import DisplayError from "../../Pages/Shared/DisplayError/DisplayError";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        errorElement: <DisplayError></DisplayError>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <Signup></Signup>
+            },
+        ]
+    }
+])
+
+export default router;
